1d61e201064d628fb693415d41e31bb073332bc1
Travis Cramer <travis.cramer@bird.co>
2019-06-03 23:47:04 -0700

    Used go fmt

--- game_test.go
+++ game_test.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
-
 func TestNewGame(t *testing.T) {
-    desiredBoardSize := 19
-    game := NewGame(desiredBoardSize)
-    if game.gameBoardSize != desiredBoardSize {
-        t.Errorf("game.gameBoardSize = %d; want %d", game.gameBoardSize, desiredBoardSize)
-    }
+	desiredBoardSize := 19
+	game := NewGame(desiredBoardSize)
+	if game.gameBoardSize != desiredBoardSize {
+		t.Errorf("game.gameBoardSize = %d; want %d", game.gameBoardSize, desiredBoardSize)
+	}
 }
 
 func TestPlacePiece(t *testing.T) {
-    game := NewGame(19)
-    game.placePiece(1, 1, game.currentPlayer)
-    if game.board[0][0] != uint8(game.currentPlayer) {
-        t.Errorf("piece on row 1 col 1 = %d; want %d", game.board[0][0], game.currentPlayer)
-    }
+	game := NewGame(19)
+	game.placePiece(1, 1, game.currentPlayer)
+	if game.board[0][0] != uint8(game.currentPlayer) {
+		t.Errorf("piece on row 1 col 1 = %d; want %d", game.board[0][0], game.currentPlayer)
+	}
 }
 
 func TestChangeTurn(t *testing.T) {
-    game := Game{currentPlayer: LIGHT}
-    game.changeTurn()
-    if game.currentPlayer != DARK {
-        t.Errorf("game.currentPlayer = %v; want %v\n", game.currentPlayer.toString(), DARK.toString())
-    }
-    game.changeTurn()
-    if game.currentPlayer != LIGHT {
-        t.Errorf("game.currentPlayer = %v; want %v\n", game.currentPlayer.toString(), LIGHT.toString())
-    }
+	game := Game{currentPlayer: LIGHT}
+	game.changeTurn()
+	if game.currentPlayer != DARK {
+		t.Errorf("game.currentPlayer = %v; want %v\n", game.currentPlayer.toString(), DARK.toString())
+	}
+	game.changeTurn()
+	if game.currentPlayer != LIGHT {
+		t.Errorf("game.currentPlayer = %v; want %v\n", game.currentPlayer.toString(), LIGHT.toString())
+	}
 }
--- player.go
+++ player.go
@@ -1,14 +1,18 @@
 package main
 
-
 type Player uint8
+
 const (
 	DARK  Player = 1
 	LIGHT Player = 2
 )
 
 func (player Player) toString() string {
-	if player == DARK { return "dark" }
-	if player == LIGHT { return "light" }
+	if player == DARK {
+		return "dark"
+	}
+	if player == LIGHT {
+		return "light"
+	}
 	return "could not get player string"
 }
