321e1295d9781ff39fc16e782172f005dcac9d5d
Travis Cramer <travis.cramer@bird.co>
2019-06-21 17:59:57 -0700

    add test

--- game_test.go
+++ game_test.go
@@ -55,5 +55,25 @@ func TestCheckForAndRemoveCapturedPieces(t *testing.T) {
     got = game.checkForAndRemoveCapturedPieces()
     assert.Equal(true, got)
 
-    assert.Equal(uint8(0), game.board[0][0])
+    assert.Equal(uint8(0), game.board[0][0])  // the piece is removed
+}
+
+func TestCheckForAndRemoveSelfCapturedPieces(t *testing.T) {
+    assert := asserter.New(t)
+
+    game := NewGame(19)
+
+    game.board[0][1] = uint8(game.currentPlayer.opposingPlayer())
+    got := game.checkForAndRemoveSelfCapturedPieces()
+    assert.Equal(false, got)
+
+    game.board[1][0] = uint8(game.currentPlayer.opposingPlayer())
+    got = game.checkForAndRemoveSelfCapturedPieces()
+    assert.Equal(false, got)
+
+    game.board[0][0] = uint8(game.currentPlayer)
+    got = game.checkForAndRemoveSelfCapturedPieces()
+    assert.Equal(true, got)
+
+    assert.Equal(uint8(0), game.board[0][0])  // the piece is removed
 }
